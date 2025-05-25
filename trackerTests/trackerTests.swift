1d84b8ea56e941395192fea76ffc59b236446176
nobu1437 <omabrirdu@yandex.ru>
2025-05-25 20:29:36 +0500

    Initial Commit

---
+++ trackerTests/trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  trackerTests.swift
+//  trackerTests
+//
+//  Created by Andrey Nobu on 25.05.2025.
+//
+
+import Testing
+@testable import tracker
+
+struct trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
