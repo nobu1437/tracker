95faef44d60718180d50854acaf8baec6c452501
nobu1437 <omabrirdu@yandex.ru>
2025-06-14 21:00:29 +0500

    fix: added final keyword

--- tracker/Views/StatisticViews/StatisticListViewController.swift
+++ tracker/Views/StatisticViews/StatisticListViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class StatisticListViewController: UIViewController {
+final class StatisticListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .green
